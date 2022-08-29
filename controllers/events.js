5596cbd9ba4de8a81f500d1886f81513397d571f
Fabian Portilla <fas_456@hotmail.com>
2022-08-29 09:58:59 -0500

    Save event to DB

--- controllers/events.js
+++ controllers/events.js
@@ -1,4 +1,5 @@
 const { response, request } = require('express')
+const Event = require('../models/Event')
 
 const getEvents = (req = request, res = response) => {
 
@@ -8,15 +9,38 @@ const getEvents = (req = request, res = response) => {
     })
 }
 
-const createEvent = (req = request, res = response) => {
+const createEvent = async (req = request, res = response) => {
 
-    res.json({
-        ok: true,
-        msg: 'Create events'
-    })
+    const { title, notes, start, end } = req.body
+    const user = req.uid
+
+    const event = new Event({ title, notes, start, end, user })
+
+    try {
+
+
+        const savedEvent = await event.save()
+
+        res.json({
+            ok: true,
+            savedEvent
+
+        })
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            msg: 'talk with the administrator'
+        })
+
+    }
+
+
 }
 
 const updateEvent = (req = request, res = response) => {
+
+
 
     res.json({
         ok: true,
